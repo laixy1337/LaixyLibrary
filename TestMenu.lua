cc7ca89fded540fd58a2ef7660256bc952790855
laixy <125830449+laixy1337@users.noreply.github.com>
2024-07-24 15:36:33 +0300

    Update TestMenu.lua

--- TestMenu.lua
+++ TestMenu.lua
@@ -90,18 +90,6 @@ local Toggle = EspTab:CreateToggle({
    CurrentValue = false,
    Flag = "EspToggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-	for _, player in ipairs(game:GetPlayers()) do
-	    if player ~= game.Players.LocalPlayer then
-
-	    local character = player.Character
-
-	    local highlight = Instance.new("Highlight")
-	    highlight.Parent = character
-	    highlight.FillColor = Color3.fromRGB(255, 0, 0)
-	    highlight.OutlineColor = Color3.fromRGB(255, 0, 0)
-	    highlight.Adornee = character
-	    end
-	end
 	print("Laix.CC")
    end,
 })
