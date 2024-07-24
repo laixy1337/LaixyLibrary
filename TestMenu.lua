e17d1821b8cdbcc9ea3d636d76a0a71032bd919b
laixy <125830449+laixy1337@users.noreply.github.com>
2024-07-24 16:01:30 +0300

    Update TestMenu.lua

--- TestMenu.lua
+++ TestMenu.lua
@@ -90,19 +90,15 @@ local Toggle = MainTab:CreateToggle({
    Flag = "Noclip1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(value)
 	if value == true then
-	   Clipon = true
-	   if not Clipon == false then
 	    for a, b in pairs(Workspace:GetChildren()) do
 			if b.Name == Plr.Name then
 			for i, v in pairs(Workspace[Plr.Name]:GetChildren()) do
 			if v:IsA("BasePart") then
 			v.CanCollide = false
 			end end end end
-	   else
-	    Stepped:Disconnect()
-	   end
+
 	elseif value == false then
-  	    Clipon = false
+  	    Stepped:Disconnect()
 	end
    end,
 })
