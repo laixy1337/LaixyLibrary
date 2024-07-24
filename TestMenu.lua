554d29227e1921a726cca6f68ba922a186bb1bb5
laixy <125830449+laixy1337@users.noreply.github.com>
2024-07-24 16:03:45 +0300

    Update TestMenu.lua

--- TestMenu.lua
+++ TestMenu.lua
@@ -89,7 +89,9 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "Noclip1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(value)
+	Stepped = game:GetService("RunService").Stepped:Connect(function()
 	if value == true then
+						
 	    for a, b in pairs(Workspace:GetChildren()) do
 			if b.Name == Plr.Name then
 			for i, v in pairs(Workspace[Plr.Name]:GetChildren()) do
@@ -100,6 +102,7 @@ local Toggle = MainTab:CreateToggle({
 	elseif value == false then
   	    Stepped:Disconnect()
 	end
+	end)
    end,
 })
 
